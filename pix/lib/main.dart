42de7c18500810b313a532228e706e4a9f1c0e28
Dulio Denis <dulio.denis@yahoo.com>
2018-09-06 14:02:17 -0400

    Added a MaterialApp Widget with a home Text Widget

--- pix/lib/main.dart
+++ pix/lib/main.dart
@@ -3,9 +3,14 @@ import 'package:flutter/material.dart';
 
 // 2. Define a 'main' function to run when the app starts
 void main() {
-// 3. Create a new text widget to show some text on the screen
+  // 3. Create a new text widget to show some text on the screen
 
-// 4. Take that widget and get it on the screen
+  // create a MaterialApp Widget that has a Text Widget as its home screen
+  var app = MaterialApp(
+    home: Text('Hello, World!'),
+  );
+
+  // 4. Take that widget and get it on the screen
 
 }
 
