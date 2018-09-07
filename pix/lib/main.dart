4fd94683a3ae9f9e3b7f2bbd8188bf4d33c4d034
Dulio Denis <dulio.denis@yahoo.com>
2018-09-07 19:44:23 -0400

    Added a Scaffold and AppBar widgets to the MaterialApp

--- pix/lib/main.dart
+++ pix/lib/main.dart
@@ -1,16 +1,13 @@
-// 1. Import a helper library from flutter to get content on the screen
 import 'package:flutter/material.dart';
 
-// 2. Define a 'main' function to run when the app starts
 void main() {
-  // 3. Create a new text widget to show some text on the screen
 
-  // create a MaterialApp Widget that has a Text Widget as its home screen
   var app = MaterialApp(
-    home: Text('Hello, World!'),
+    home: Scaffold(
+      appBar: AppBar(),
+    ),
   );
 
-  // 4. Take that widget and get it on the screen
   runApp(app);
 }
 
