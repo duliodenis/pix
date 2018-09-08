ed665dedad20852a2f7e2c9e46af6143b562d6ac
Dulio Denis <dulio.denis@yahoo.com>
2018-09-08 16:27:23 -0400

    Refactored the App StatelessWidget into a StatefulWidget

--- pix/lib/src/app.dart
+++ pix/lib/src/app.dart
@@ -1,22 +1,28 @@
-// 1 - Import flutter helper library
 import 'package:flutter/material.dart';
 
-// 2 - Create a class that will be our custom widget
-//     This class must extend the 'StatelessWidget' base class
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
 
-  // 3 - Must define a build method that returns the widgets that
-  //     this widget will show that takes a context parameter
+class AppState extends State<App> {
+  // instance variable to track the tap counts
+  int counter = 0;
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
+        body: Text('$counter taps'),
         appBar: AppBar(
           title: Text('Lets see some images!'), 
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
-            print('Floating Action Button Tapped');
+            setState(() {
+              counter++;
+            });
           },
         ),
       ),
