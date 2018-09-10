ed71a7086abd6f49534e345bc9b667bec3c46139
Dulio Denis <dulio.denis@yahoo.com>
2018-09-09 20:47:32 -0400

    Added an ImageList custom widget to show a ListView

--- pix/lib/src/app.dart
+++ pix/lib/src/app.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
+import 'models/image_model.dart';
+import 'widgets/image_list.dart';
+
 
 class App extends StatefulWidget {
   createState() {
@@ -9,21 +14,28 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   // instance variable to track the tap counts
   int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    final response = await get('http://jsonplaceholder.typicode.com/photos/$counter');
+    final imageModel = ImageModel.fromJSON(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
 
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text('$counter taps'),
+        body: ImageList(images),
         appBar: AppBar(
           title: Text('Lets see some images!'), 
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              counter++;
-            });
-          },
+          onPressed: fetchImage,
         ),
       ),
     );
